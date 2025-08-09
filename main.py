1fc1505d39811e6fd4d0a74d39443bca6a8e76d3
Gustavo Barreto <Gugacontas1303@gmail.com>
2025-08-09 16:12:00 -0300

    REFACTOR: update code and UI

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import tkinter as tk
 import subprocess
 import sys
-import os
+import threading
 
-# Caminho absoluto para o Python do ambiente virtual
 python_path = sys.executable
 
-def exec_capture_gesture():
-    subprocess.Popen([python_path, "gesture_capture.py"])
+def run_script(script_name , action_description):
 
-def exec_train_model():
-    subprocess.Popen([python_path, "train_model.py"])
+    for button in buttons:
+        button.config(state='disabled')
+    status_label.config(text=f"Currently {action_description}...")
 
-def exec_gesture_recognition():
-    subprocess.Popen([python_path, "gesture_recognition.py"])
+    def run_archive():
+        subprocess.run([python_path, script_name])
+        window.after(0, restore_interface_state)
 
-# Criar janela
+    threading.Thread(target=run_archive).start()
+
+def restore_interface_state():
+    for button in buttons:
+        button.config(state='normal')
+    status_label.config(text="Select an option below:")
+
 window = tk.Tk()
 window.title("Gesture Recognition Controller")
 width_window = 400
@@ -29,10 +35,16 @@ pos_y = (height_screen // 2) - (height_window // 2)
 
 window.geometry(f"{width_window}x{height_window}+{pos_x}+{pos_y}")
 
-# Botões
-tk.Button(window, text="Capture Gesture", command=exec_capture_gesture).pack(pady=10)
-tk.Button(window, text="Train Model", command=exec_train_model).pack(pady=10)
-tk.Button(window, text="Recognize Gestures", command=exec_gesture_recognition).pack(pady=10)
+status_label = tk.Label(window, text='Select an option below:')
+status_label.pack(pady=10)
 
-# Iniciar janela
+buttons =   [
+            tk.Button(window, text="Capture Gesture", command=lambda: run_script("gesture_capture.py" , "Capturing gesture")),
+            tk.Button(window, text="Train Model", command=lambda: run_script("train_model.py" , "training model")),
+            tk.Button(window, text="Recognize Gestures", command=lambda: run_script("gesture_recognition.py" , "recognition gesture"))
+            ]
+
+for button in buttons:
+    button.pack(pady=5)
+
 window.mainloop()
